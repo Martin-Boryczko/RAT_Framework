2ab9ce25823e6fc25a536ce787f821b6a2d0f057
Martin-Boryczko <114809618+Martin-Boryczko@users.noreply.github.com>
2022-10-01 14:45:59 +0200

    Mock server draft

--- http_mock_server/http_mock_sample_server.py
+++ http_mock_server/http_mock_sample_server.py
@@ -1,2 +1,34 @@
+from http import HTTPStatus
+
+from flask import Flask
+
+
 class HttpMockSampleServer:
-    pass
+    def __init__(self):
+        app = Flask(__name__)
+
+        @app.route('/sample/person/', methods=['GET'])
+        def get_sample_person():
+            return "/sample/person GET method!", HTTPStatus.OK
+
+        @app.route('/sample/person/', methods=['POST'])
+        def post_sample_person():
+            return "/sample/person POST method!", HTTPStatus.CREATED
+
+        @app.route('/sample/person/', methods=['PUT'])
+        def put_sample_person():
+            return "/sample/person PUT method!", HTTPStatus.ACCEPTED
+
+        @app.route('/sample/person/', methods=['PATCH'])
+        def patch_sample_person():
+            return "/sample/person PATCH method!", HTTPStatus.ACCEPTED
+
+        @app.route('/sample/person/', methods=['DELETE'])
+        def delete_sample_person():
+            return "/sample/person DELETE method!", HTTPStatus.OK
+
+        @app.route('/', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+        def error_sample_person():
+            return "Hack your brain!", HTTPStatus.BAD_REQUEST
+
+        app.run(host='127.0.0.1', port=105)
